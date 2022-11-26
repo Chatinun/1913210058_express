a3b7a0d2f2188c6f7745a71822159261c0cb56d2
Chatinun <nu.chatinun_st@tni.ac.th>
2022-11-26 11:20:06 +0700

    Workshop A

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Hello');
+  res.status(200).json({
+    fullname: 'Chatinun Nuntapasuk'
+  })
 });
+
+router.get('/bio',function(req, res, next){
+  res.status(200).json({
+    fullname: 'Chatinun Nuntapasuk',
+    nickname: 'Max',
+    hobby: 'Sleep',
+    gitusername: 'Chatinun'  
+  })
+})
 
 module.exports = router;
