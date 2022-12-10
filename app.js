4386674398e7adef16acf52a0146c0a7a6d58fab
Chatinun <nu.chatinun_st@tni.ac.th>
2022-12-10 11:09:12 +0700

    mongoDB company

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var companyRouter = require('./routes/company');
 
 var app = express();
+
+mongoose.connect('mongodb+srv://chatinun:SwxOVC7lSMqdaYu0@1913210058-max.x1dnkvw.mongodb.net/restfulapi?retryWrites=true&w=majority',
+                {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(logger('dev'));
 app.use(express.json());
--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,30 +1,10 @@
-exports.index = (req, res, next) => {
+const Company = require('../models/company');
+
+exports.index = async (req, res, next) => {
+
+  const company = await Company.findOne();
+
   res.status(200).json({
-    data: [
-      {
-        id: 1,
-        name: "Bitegg Inc.",
-        address: {
-            province: "Chiang Mai",
-            postcode : 50100
-        }
-      },
-      {
-        id: 2,
-        name: "Line",
-        address: {
-            province: "Bangkok",
-            postcode : 10330
-        }
-      },
-      {
-        id: 3,
-        name: "Shopee",
-        address: {
-            province: "Bangkok",
-            postcode : 10310
-        }
-      }
-    ],
+    data: company,
   });
 };
