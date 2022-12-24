773c33eace4f4a4993f8d71d84a9d4788fb4b945
Chatinun <nu.chatinun_st@tni.ac.th>
2022-12-24 10:54:16 +0700

    Workshop B

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,9 +1,29 @@
 const Shop = require("../models/shop");
+const Menu = require("../models/menu");
 
 exports.index = async (req, res, next) => {
-    const shop = await Shop.find();
+  const shops = await Shop.find()
+    .select("name photo location")
+    .sort({ _id: -1 });
 
-    res.status(200).json({
-      data: shop,
-    });
+  const shopWithPhotoDomain = shops.map((shop,index) => {
+    return {
+        id: shop._id,
+        name: shop.name,
+        photo: 'http://localhost:3000/images/' + shop.photo,
+        location: shop.location,
+    };
+  });
+
+  res.status(200).json({
+    data: shopWithPhotoDomain,
+  });
 };
+
+exports.menu = async (req, res, next) => {
+    const menus = await Menu.find();
+  
+    res.status(200).json({
+      data: menus,
+    });
+  };
--- routes/shop.js
+++ routes/shop.js
@@ -6,4 +6,6 @@ const shopController = require('../controllers/shopController')
 /* GET users listing. */
 router.get('/', shopController.index);
 
+router.get('/menu', shopController.menu);
+
 module.exports = router;
