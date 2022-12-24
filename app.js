f3f3f4d9c8b260831b5be20704faefe5642cbe91
Chatinun <nu.chatinun_st@tni.ac.th>
2022-12-24 09:49:04 +0700

    Workshop A

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var companyRouter = require('./routes/company');
 const staffRounter = require('./routes/staff');
+const shopRounter = require('./routes/shop');
 
 var app = express();
 
@@ -24,5 +25,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/company', companyRouter);
 app.use('/staff', staffRounter);
+app.use('/shop',shopRounter)
 
 module.exports = app;
