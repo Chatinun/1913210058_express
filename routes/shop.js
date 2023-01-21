550965dfdc6165c517953040d369aa0298b85046
Chatinun <nu.chatinun_st@tni.ac.th>
2023-01-21 10:08:36 +0700

    Workshop A

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const shopController = require('../controllers/shopController')
+const { body } = require("express-validator");
 
 /* GET users listing. */
 router.get('/', shopController.index);
@@ -10,6 +11,13 @@ router.get('/menu', shopController.menu);
 
 router.get('/:id', shopController.shopmenu);
 
-router.post('/', shopController.insert);
+router.post('/',
+[
+    body("name").not().isEmpty().withMessage("กรุณาป้อนชื่อสกุลด้วย"),
+    body("location").not().isEmpty().withMessage("กรุณาป้อนสถานที่ด้วย"),
+    body("location.lat").isNumeric().withMessage("กรุณากรอกเป็นตัวเลข"),
+    body("location.lgn").isNumeric().withMessage("กรุณากรอกเป็นตัวเลข")
+]
+, shopController.insert);
 
 module.exports = router;
--- routes/staff.js
+++ routes/staff.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 const staffController = require('../controllers/staffController')
+const { body } = require("express-validator");
 
 router.get('/', staffController.index);
 
 router.get('/:id', staffController.show);
 
-router.post('/', staffController.insert);
+router.post('/' ,
+[
+    body("name").not().isEmpty().withMessage('กรุณาป้อนชื่อสกุลด้วย'),
+    body("salary").not().isEmpty().withMessage("กรุณาป้อนรายได้ด้วย").isNumeric().withMessage('กรุณาป้อนรายได้เป็นตัวเลข')
+]
+, staffController.insert);
 
 router.delete('/:id', staffController.destroy);
 
